75f915ec319f2848b717709fe433646b6599ef06
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-10-11 18:34:49 +0700

    Added headless UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,68 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 
-function App() {
-  const [count, setCount] = useState(0)
+const people = [{ name: 'LL' }, { name: 'DX' }, { name: 'DZ' }];
+
+export default function App() {
+  const [selected, setSelected] = useState(people[0]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">Hello Vite + React!!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='bg-green-600 h-screen flex flex-col items-center'>
+      <h1 className='mt-32 text-white text-2xl font-bold'>Ladaa</h1>
+      <h2 className='text-white'>React + Vite + Tailwind + Headless UI</h2>
+      <div className='w-2/5 mx-auto'>
+        <Listbox value={selected} onChange={setSelected}>
+          <div className='relative mt-1'>
+            <Listbox.Button className='relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm'>
+              <span className='block truncate'>{selected.name}</span>
+              <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
+                <SelectorIcon
+                  className='w-5 h-5 text-gray-400'
+                  aria-hidden='true'
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave='transition ease-in duration-100'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'>
+              <Listbox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+                {people.map((person, personIdx) => (
+                  <Listbox.Option
+                    key={personIdx}
+                    className={({ active }) =>
+                      `${active ? 'bg-green-400 text-white' : 'text-gray-900'}
+                          cursor-pointer select-none relative py-2 pl-10 pr-4`
+                    }
+                    value={person}>
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`${
+                            selected ? 'font-medium' : 'font-normal'
+                          } block truncate`}>
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`${
+                              active ? 'text-amber-600' : 'text-amber-600'
+                            }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}>
+                            <CheckIcon className='w-5 h-5' aria-hidden='true' />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
