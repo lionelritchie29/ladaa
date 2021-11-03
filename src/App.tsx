7259e1fdc511a5cf06351dde9f6eecfea327e3fb
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-11-03 20:47:20 +0700

    Add register page and login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/_Layout';
 import ModalProvider from './contexts/ModalContext';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import MealPlan from './pages/MealPlan';
 import RecipeDetail from './pages/RecipeDetail';
@@ -14,6 +16,12 @@ export default function App() {
       <ModalProvider>
         <Layout>
           <Switch>
+            <Route path='/auth/login'>
+              <Login />
+            </Route>
+            <Route path='/auth/register'>
+              <Register />
+            </Route>
             <Route path='/recipes/:id'>
               <RecipeDetail />
             </Route>
