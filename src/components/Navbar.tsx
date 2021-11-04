cd7820668a22fc4754379573b0eb4ac25eb45608
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-11-04 20:17:58 +0700

    Add login and register link in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,16 +34,16 @@ const Navbar = () => {
                   className='text-gray-100 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                   Meal Plan
                 </Link>
-                {/* <a
-                  href='#'
+                <Link
+                  to='/auth/login'
                   className='text-gray-100 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                   Login
-                </a>
-                <a
-                  href='#'
+                </Link>
+                <Link
+                  to='/auth/register'
                   className='text-gray-100 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                   Register
-                </a> */}
+                </Link>
               </div>
             </div>
           </div>
@@ -115,24 +115,6 @@ const Navbar = () => {
           </div>
           <div className='hidden lg:block lg:ml-4'>
             <div className='flex items-center'>
-              <button className='flex-shrink-0 bg-green-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
-                <span className='sr-only'>View notifications</span>
-                <svg
-                  className='h-6 w-6 text-gray-100'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                  aria-hidden='true'>
-                  <path
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
-                  />
-                </svg>
-              </button>
-
               {/* <!-- Profile dropdown --> */}
               <div className='ml-4 relative flex-shrink-0'>
                 <div>
@@ -200,16 +182,16 @@ const Navbar = () => {
                 className='text-gray-100 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
                 Meal Plan
               </Link>
-              {/* <a
-                href='#'
+              <Link
+                to='/auth/login'
                 className='text-gray-100 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
                 Login
-              </a>
-              <a
-                href='#'
+              </Link>
+              <Link
+                to='/auth/register'
                 className='text-gray-100 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
                 Register
-              </a> */}
+              </Link>
             </div>
             <div className='pt-4 pb-3 border-t border-green-700'>
               <div className='flex items-center px-5'>
